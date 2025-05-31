87ed497a40ce01a772d42b6abff95a3d264d1446
lepagejeremi <jeremilepage007@gmail.com>
2025-05-30 20:54:13 -0400

    creation et test du glossaire, ca fonctionne wouhou

--- ExTraductionBabeldown.R
+++ ExTraductionBabeldown.R
@@ -8,6 +8,14 @@ install.packages('babeldown', repos = c('https://ropensci.r-universe.dev', 'http
 
 Sys.setenv(DEEPL_API_KEY = "3be8c59d-9e13-4022-a7d8-30cd5bf3a9b6:fx")
 
-babeldown::deepl_translate("C:/Users/lepj1/Downloads/README (1).md", source_lang = "EN", target_lang = "FR",out_path = "C:/Users/lepj1/Downloads/README_translated.md")
+babeldown::deepl_translate("C:/Users/lepj1/Downloads/README (1).md", source_lang = "EN", target_lang = "FR",out_path = "C:/Users/lepj1/Downloads/README_translated.md",glossary_name = "animint-manual-glossaire-fr-en")
 
 ```
+
+if (FALSE) { # \dontrun{
+  babeldown::deepl_upsert_glossary(
+    filename = "animint-manual-glossaire-fr-en.csv",
+    target_lang = "FR",
+    source_lang = "EN"
+  )
+} # }
