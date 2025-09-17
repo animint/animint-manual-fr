5565b3d4ab72f0595c7aabfa420ed95f9c7b7034
Toby Dylan Hocking <toby.dylan.hocking@usherbrooke.ca>
2025-09-17 10:15:26 -0400

    rm auth_token

--- render.R
+++ render.R
@@ -1,6 +1,6 @@
 options(repos="http://cloud.r-project.org")
 for(p in c("nc","quarto","animint2","lars"))if(!requireNamespace(p))install.packages(p)
-remotes::install_github(c("animint/animint2","animint/animint2fr"), auth_token=NULL)
+remotes::install_github(c("animint/animint2","animint/animint2fr"))
 qmd_vec <- Sys.glob("Chapitres/Ch*/Ch*_source.qmd")
 for(qmd_i in seq_along(qmd_vec)){
   qmd_in <- qmd_vec[[qmd_i]]
